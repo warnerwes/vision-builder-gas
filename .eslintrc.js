1e75f999264abeadd54d677e9564304e547a9080
wesleymwarner@gmail.com <wesleymwarner@gmail.com>
2025-09-10 10:38:02 -0700

    Update ESLint configuration for Google Apps Script: disable camelcase rule, change several rules to warnings for better development experience, and add common GAS function names as readonly to improve linting accuracy.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,44 +22,42 @@ module.exports = {
     }],
     'require-jsdoc': 'off', // GAS functions often don't need JSDoc
     'valid-jsdoc': 'off',
-    'camelcase': ['error', {
-      allow: ['^[A-Z][a-zA-Z0-9_]*$', '^[a-z]+_[a-z_]+$'], // Allow CONSTANT_CASE and snake_case for GAS
-      properties: 'never',
-    }],
-    'no-unused-vars': ['error', {
+    'camelcase': 'off', // Disable camelcase for GAS snake_case functions
+    'no-unused-vars': ['warn', {
       argsIgnorePattern: '^_', // Allow unused params starting with _
       varsIgnorePattern: '^_',
+      ignoreRestSiblings: true,
     }],
-    'no-undef': 'error',
+    'no-undef': 'warn', // Change to warning since GAS functions are defined in other files
     'no-console': 'off', // GAS doesn't have console, but allow for debugging
-    'prefer-const': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
-    'prefer-template': 'error',
-    'template-curly-spacing': 'error',
-    'arrow-spacing': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single', { avoidEscape: true }],
-    'indent': ['error', 2],
-    'space-before-function-paren': ['error', 'never'],
-    'keyword-spacing': 'error',
-    'space-infix-ops': 'error',
-    'eol-last': 'error',
-    'no-trailing-spaces': 'error',
-    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
-    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'curly': ['error', 'all'],
-    'eqeqeq': ['error', 'always'],
+    'prefer-const': 'warn', // Change to warning for better development experience
+    'no-var': 'warn', // Change to warning
+    'object-shorthand': 'warn', // Change to warning
+    'prefer-template': 'warn', // Change to warning
+    'template-curly-spacing': 'warn', // Change to warning
+    'arrow-spacing': 'warn', // Change to warning
+    'comma-dangle': ['warn', 'always-multiline'], // Change to warning
+    'semi': ['warn', 'always'], // Change to warning
+    'quotes': ['warn', 'single', { avoidEscape: true }], // Change to warning
+    'indent': ['warn', 2], // Change to warning
+    'space-before-function-paren': ['warn', 'never'], // Change to warning
+    'keyword-spacing': 'warn', // Change to warning
+    'space-infix-ops': 'warn', // Change to warning
+    'eol-last': 'warn', // Change to warning
+    'no-trailing-spaces': 'warn', // Change to warning
+    'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 1 }], // Change to warning
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }], // Change to warning
+    'curly': ['warn', 'all'], // Change to warning
+    'eqeqeq': ['warn', 'always'], // Change to warning
     'no-eval': 'error',
     'no-implied-eval': 'error',
     'no-new-func': 'error',
     'no-script-url': 'error',
-    'no-alert': 'error',
-    'no-empty': 'error',
-    'no-extra-boolean-cast': 'error',
-    'no-extra-semi': 'error',
-    'no-regex-spaces': 'error',
+    'no-alert': 'warn', // Change to warning
+    'no-empty': 'warn', // Change to warning
+    'no-extra-boolean-cast': 'warn', // Change to warning
+    'no-extra-semi': 'warn', // Change to warning
+    'no-regex-spaces': 'warn', // Change to warning
     'no-unreachable': 'error',
     'use-isnan': 'error',
     'valid-typeof': 'error',
@@ -105,5 +103,29 @@ module.exports = {
     'ScriptProperties': 'readonly',
     'DocumentProperties': 'readonly',
     'console': 'readonly', // For debugging
+    // Common GAS function names that are defined across files
+    'getMe_': 'readonly',
+    'readRows_': 'readonly',
+    'writeRow_': 'readonly',
+    'updateOrInsert_': 'readonly',
+    'updateOrInsertMany_': 'readonly',
+    'deleteRowById_': 'readonly',
+    'deleteRowsWhere_': 'readonly',
+    'sheet_': 'readonly',
+    'getHeaders_': 'readonly',
+    'uid_': 'readonly',
+    'getCurrentUserEmail': 'readonly',
+    'verifyIdToken_': 'readonly',
+    'ensureVisionSheet_': 'readonly',
+    'ensureAdminSheets_': 'readonly',
+    'SHEET_IDS': 'readonly',
+    'DB_SHEET_NAME': 'readonly',
+    'CLASSNAME': 'readonly',
+    'MISSIONNAME': 'readonly',
+    'USERNAME': 'readonly',
+    'OAUTH_CLIENT_ID': 'readonly',
+    'OPENAI_MODEL': 'readonly',
+    'OPENAI_URL': 'readonly',
+    'Classroom': 'readonly', // Google Classroom API
   },
 };
